eaa301534206fd926ccf19f2f33445e58030d7a8
karumank <karumank@mail.gvsu.edu>
2016-04-18 01:08:50 +0000

    Migration updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160417032612) do
+ActiveRecord::Schema.define(version: 20150723052743) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
